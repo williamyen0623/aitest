06e4a3882e6534b07d214fc2b7925ebdb28586aa
williamyen0623 <a0988977340@gmail.com>
2022-10-29 13:34:06 +0800

    update

--- test.py
+++ test.py
@@ -1,5 +1,5 @@
 import facebook
-graph = facebook.GraphAPI(access_token='EAAGwg103BWkBAKkDsP1r07BtUM87Dv6TkJnmMhyZA0CScNBpOYHZCoLfAmkopSTrgRL0FiLupH4AMBimE1BRqZCFzgaZAnxiRzRRTIKgWgmYgyFXxVnctV131ct03bO8NIJqJIg7ZCOjZBZBteg7jscO8xhoQaY9WOIKD3pXoZABSao4dUV0yneMRc3OsircuzYCs78weCKJNvmuqOkW935DO2DMzcwSAMY5vgBC2IZBs9KGsENY4Ce3e', version='3.1')
+graph = facebook.GraphAPI(access_token='', version='3.1')
 
 # Post something!
    # Add a link and write a message about it.
